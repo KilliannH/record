7d9022955e9dd1f9d9f19ce2e4c647de23fe934f
KilliannH <killiannhervagault@gmail.com>
2019-05-06 00:21:15 +0200

    all working

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 // Imports modules.
-const fs = require(`fs`),
-    path = require(`path`);
+const fs = require(`fs`);
+const path = require(`path`);
+const Lame = require('lame');
+const Speaker = require('speaker');
+const { exec } = require('child_process');
 
 const AudioRecorder = require(`node-audiorecorder`);
 const xevEmitter = require('xev-emitter')(process.stdin);
-
-const DIRECTORY = `examples-recordings`;
 
 // Initialize recorder and file stream.
 const audioRecorder = new AudioRecorder({
@@ -14,16 +15,14 @@ const audioRecorder = new AudioRecorder({
     keepSilence: false   // Keep the silence in the recording.
 }, console);
 
-// Create path to write recordings to.
-if (!fs.existsSync(DIRECTORY)){
-    fs.mkdirSync(DIRECTORY);
-}
 // Create file path with random name.
-const fileName = path.join(DIRECTORY, 'record').concat(`.wav`);
-console.log(`filename : `, fileName);
+const fileName = path.join(__dirname, '../', 'appointment-ai-v2/', 'record').concat('.wav');
 
 // Create write stream.
 let fileStream = null;
+
+let ding = fs.createReadStream('ding.mp3');
+
 
 process.stdin.resume();
 
@@ -38,7 +37,21 @@ xevEmitter.on('KeyPress', (key) => {
 xevEmitter.on('KeyRelease', (key) => {
     console.log(key, 'was released');
     if(key === 'ISO_Level3_Shift') {
-        audioRecorder.stop();
-        fileStream = null;
+        setTimeout(() => {
+
+            audioRecorder.stop();
+            fileStream = null;
+
+            var a = exec('cd ~/Documents/appointment-ai-v2 && python3 main.py');
+            ding.pipe(new Lame.Decoder())
+                .on('format', function (format) {
+                    this.pipe(new Speaker(format));
+                });
+
+            process.on('exit', function () {
+                a.kill();
+            });
+
+        }, 1500);
     }
 });
